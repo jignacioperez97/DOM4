a9ed8f006c8921b5cffd024450b0a0433e9996b3
Ignacio <jignacioperez97@gmail.com>
2022-08-16 20:23:52 -0300

    archivos cargados

---
+++ js/dom3.js
@@ -0,0 +1,20 @@
+/* 3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea 
+y un botón que al ser presionado agregue dicha tarea a una lista,
+ cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin.  */
+
+let formulario = document.getElementById("#formularioTarea");
+/* formulario.addEventListener("submit", enviarTarea); */
+
+let arrayTareas = [];
+
+{
+  /* <div class="container mt-5 listaTareas">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div> */
+}
